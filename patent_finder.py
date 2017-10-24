9646f4d4b5e64dcecb63315a9f746c1403fd0c75
Inhye <inb125@mail.harvard.edu>
2017-10-23 19:58:17 -0700

    get rid of duplicate patent numbers

--- patent_finder.py
+++ patent_finder.py
@@ -6,14 +6,16 @@ def open_and_read_file(filename):
     patent_numbers = []
     with open(filename) as data:
         for line in data:
-            patent_numbers.append(line)
+            # no duplicates 
+            if line not in patent_numbers:
+                patent_numbers.append(int(line))     
     return patent_numbers
 
 # pass in the file with list of patent numbers
 patent_numbers = open_and_read_file('patent_num.txt')
 
 # randomly choose a patent number from the list 
-patent_number = choice(patent_numbers)
+patent_number = str(choice(patent_numbers))
 
 # generate link to that patent 
 link = "http://appft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.html&r=1&p=1&f=G&l=50&d=PG01&S1=" + patent_number + "&OS=" + patent_number + "&RS=" + patent_number
